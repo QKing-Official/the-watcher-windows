c779c2deb13eabfafe02299d5745e24dd2cf9f46
QKing <162024807+QKing-Official@users.noreply.github.com>
2025-02-24 16:11:03 +0100

    Update dev.js

--- dev.js
+++ dev.js
@@ -1,14 +1,18 @@
-import { spawn } from "child_process";
-
-const devProcess = spawn("npm", ["run", "dev"], { stdio: "inherit" });
-
+import spawn from "cross-spawn";
 import start from "./src/loop.js";
-await start();
 
-devProcess.on("close", (code) => {
-  console.log(`Dev process exited with code ${code}`);
-});
+async function run() {
+  const devProcess = spawn("npm", ["run", "dev"], { stdio: "inherit" });
 
-devProcess.on("error", (error) => {
-  console.error("Error running npm command:", error.message);
-});
+  await start();
+
+  devProcess.on("close", (code) => {
+    console.log(`Dev process exited with code ${code}`);
+  });
+
+  devProcess.on("error", (error) => {
+    console.error("Error running npm command:", error.message);
+  });
+}
+
+run();
